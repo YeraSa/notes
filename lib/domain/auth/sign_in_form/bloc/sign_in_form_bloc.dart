d4f60f4d4d2a017d13d7d81a6fd12fbcd5109976
YeraSa <sa.yeraly@gmail.com>
2022-08-08 14:55:06 +0600

    bloc setup

---
+++ lib/domain/auth/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:ffi';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../auth_failures.dart';
+import '../../i_auth_facade.dart';
+import '../../value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    //TODO: implement
+  }
+}
